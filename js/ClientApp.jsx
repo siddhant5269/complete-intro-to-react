afb28c01e09fec3d276af366b6fd31f25a91996c
Siddhant Kumar <siddhant5269@outlook.com>
2017-08-02 18:48:13 +0530

    'changedstuff''

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,25 @@
+import React from "react";
+import { render } from "react-dom";
+
 const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  return ce(
+    "div",
+    null,
+    ce("h1", { style: { color: props.color } }, props.title)
+  );
 };
 
 const MyFirstComponent = function() {
   return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+    "div",
+    { id: "my-first-component" },
+    ce(MyTitle, { title: "Game of Thrones", color: "YellowGreen" }),
+    ce(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "LimeGreen" }),
+    ce(MyTitle, { title: "Silicon Valley", color: "peru" })
   );
 };
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById("app"));
---
+++ js/test.js
@@ -0,0 +1,8 @@
+const testObject = {
+  testProp1: "testProp1",
+  testProp2: "testProp2"
+};
+
+const testObject2 = testObject
+testObject2.testProp3 = "testProp3"
+console.log(testObject)
